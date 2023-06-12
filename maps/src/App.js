d1af8d9183b6c4e21dcdb8f4f63bb162b2aff193
Pranali Bhasme <pranali.b@whizit.co.in>
2023-06-12 14:24:59 +0530

    updated map code

---
+++ maps/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Mapcontainer from './component/mapcontainer/mapcontainer'
+
+const App = () => {
+  return (
+    <div>
+    <Mapcontainer/>
+    </div>
+  )
+}
+
+export default App
